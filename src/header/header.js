3a6ea1eddce7ae25c2af691b6411c2a8a3b7ad8d
Igor Sidnev <mr.igor.sidnev@gmail.com>
2023-01-02 22:56:08 +0200

    Clean dist folder for each file change

--- src/header/header.js
+++ src/header/header.js
@@ -3,7 +3,7 @@ import "./header.scss";
 class Header {
   render() {
     const h1 = document.createElement("h1");
-    h1.innerHTML = "Heading";
+    h1.innerHTML = "Heading title";
     document.querySelector("body").appendChild(h1);
   }
 }
