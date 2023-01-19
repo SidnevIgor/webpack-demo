dd55812a6e38100ff1e19ed9dbb000a0428e1209
Igor Sidnev <mr.igor.sidnev@gmail.com>
2023-01-19 15:47:30 +0200

    Spliting production and development builds

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,23 @@
 const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
   },
-  mode: "none",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
-    new TerserPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "styles.[contenthash].css",
-    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "Webpack demo",
---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.[contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack demo",
+      meta: {
+        description: "Some description",
+      },
+    }),
+  ],
+};
